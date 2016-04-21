959d228ee0664c8eb4ed7a27e62df927b09b5d9a
bai-yingzhao <ts-bai.yingzhao@linkshare.com>
2016-04-21 10:44:36 +0800

    commit

---
+++ src/com/bai/test/Tdd.java
@@ -0,0 +1,11 @@
+package com.bai.test;
+
+public class Tdd {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Integer a = null;
+        System.out.println(a == 3);
+    }
+
+}
